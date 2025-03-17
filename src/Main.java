38ccd8a0ce6296b363fa851d35f3d0cbb786e001
Tlha_Caliskan <ahmetalhacaliskan@gmail.com>
2025-03-17 21:10:43 +0300

    talha

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,8 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("sa");
         System.out.println("as");
+        System.out.println("sa");
+        System.out.println("as");
 
     }
 }
